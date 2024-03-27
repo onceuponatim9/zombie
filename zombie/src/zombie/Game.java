6921caf7e25c28b296f9a6e4c4c8a3f820ca8732
onceuponatim9 <sjung8018a@gmail.com>
2024-03-27 12:55:39 +0900

    Update : Boss attack()

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -26,6 +26,8 @@ abstract class Unit {
 	}
 	public void setHp(int hp) {
 		this.hp = hp;
+		if(this.hp < 0)
+			this.hp = 0;
 	}
 	public int getMax() {
 		return max;
@@ -83,16 +85,32 @@ class Zombie extends Unit {
 	}
 }
 
-class Boss extends Unit {
-	private int shield;
+class Boss extends Zombie {
+	private int shield, power;
 	
 	public Boss(int pos, int hp, int max, int shield) {
 		super(pos, hp, max);
 		this.shield = shield;
 	}
 	
-	public void attack(Unit unit) {
+	public void attack(Unit hero) {
+		int heroHp = hero.getHp();
+		int bossHp = this.getHp();
 		
+		int rNum = ran.nextInt(4);
+		if(rNum == 0) {
+			power = 2 * ran.nextInt(max) + 1;
+			System.out.println("boss의 필살기");
+		}
+		else
+			power = ran.nextInt(max) + 1;
+		
+		hero.setHp(hero.getHp() - power);
+		this.setHp(this.getHp() + power);
+		
+		System.out.printf("boss가 %d의 power로 공격\n", power);
+		System.out.printf("hero의 hp : %d -> %d\n", heroHp, hero.getHp());
+		System.out.printf("boss의 hp : %d -> %d\n", bossHp, this.getHp());
 	}
 }
 
